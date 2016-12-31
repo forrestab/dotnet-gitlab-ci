5c9da30f7667e1d0576791f414c21a2c13202790
Forrest Brown <forrestabrown@gmail.com>
2016-12-31 15:47:16 -0500

    Added solution

---
+++ NewTypes/src/NewTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using NewTypes.Pets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewTypes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<IPet> pets = new List<IPet>
+            {
+                new Dog(),
+                new Cat()
+            };
+
+            foreach (var pet in pets)
+            {
+                Console.WriteLine(pet.TalkToOwner());
+            }
+        }
+    }
+}
---
+++ NewTypes/test/NewTypes.Tests/PetTests.cs
@@ -0,0 +1,30 @@
+﻿using NewTypes.Pets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NewTypes.Tests
+{
+    public class PetTests
+    {
+        [Fact]
+        public void DogTalkToOwnerTest()
+        {
+            string expected = "Woof!";
+            string actual = new Dog().TalkToOwner();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CatTalkToOwner()
+        {
+            string expected = "Meow!";
+            string actual = new Cat().TalkToOwner();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
